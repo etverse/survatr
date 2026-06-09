8bbf4c32c367d981525cdef2570d420e717620dc
Lorenzo Fabbri <lorenzo.fabbri92sm@gmail.com>
2026-06-09 14:51:47 +0200

    chore(diagnose): pin chunk 10 hash; add .I to data.table importFrom
    - SURVIVAL_PACKAGE_HANDOFF.md + CHUNK_10_DIAGNOSE.md: pin commit 75bbe1f
    - R/survatr-package.R: add .I to @importFrom data.table (used by
      diag_weights to extract first-row-per-id indices)

--- R/survatr-package.R
+++ R/survatr-package.R
@@ -1,5 +1,7 @@
 #' @keywords internal
-#' @importFrom data.table := .N .SD data.table is.data.table as.data.table copy setkeyv shift
+#' @importFrom data.table := .N .SD .I
+#' @importFrom data.table data.table is.data.table as.data.table
+#' @importFrom data.table copy setkeyv shift
 #' @importFrom rlang abort warn inform
 #' @importFrom numDeriv jacobian
 #' @importFrom sandwich bread estfun
